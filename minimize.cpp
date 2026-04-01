8f600d74e23790930ce747f4ca995c2b413d244e
Abhilash30 <Abhilashpawar14@gmail.com>
2026-04-02 01:10:08 +0530

    Day 18

---
+++ minimize.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,d;
+        cin>>a;
+        cin>>b;
+        int c = b - a;
+        
+        cout<<c<<"\n";
+    }
+   
+
+}
